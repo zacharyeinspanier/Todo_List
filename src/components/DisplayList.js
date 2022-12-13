5ff50a087f586aabaaab371c73bed3696cdec111
Zach Einspanier <zeinspanier@wisc.edu>
2022-12-13 15:40:34 -0600

    Hide Add item

--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -1,18 +1,21 @@
-import React, {useRef} from "react";
+import React from "react";
 import Add from "./Add";
 import Delete from "./Delete";
 import "../styles/DisplayList.css";
 
 const DisplayList = ({ selectedList, addListItem, deletListItem }) => {
+  var addItemClassName = "";
+  if (selectedList.name === "") {
+    addItemClassName = "hide";
+  } else {
+    addItemClassName = "";
+  }
 
-
-  function strikeItem(event){
-
-    if(event.target.className === ''){
-      event.target.className = 'strikeThrough'
-    }
-    else{
-      event.target.className = '';
+  function strikeItem(event) {
+    if (event.target.className === "") {
+      event.target.className = "strikeThrough";
+    } else {
+      event.target.className = "";
     }
     return;
   }
@@ -27,7 +30,7 @@ const DisplayList = ({ selectedList, addListItem, deletListItem }) => {
               <li
                 id={element + index}
                 key={element + index}
-                onClick={event => strikeItem(event)}
+                onClick={(event) => strikeItem(event)}
               >
                 {element}
               </li>
@@ -35,7 +38,7 @@ const DisplayList = ({ selectedList, addListItem, deletListItem }) => {
             </div>
           );
         })}
-        <li>
+        <li className={addItemClassName}>
           <Add addListOrItem={addListItem} placeHolderTxt="Enter Item Name" />
         </li>
       </ul>
